49ab7bf909e334928a00212f7a715937c563cec9
Jared Edler <jared.edler@bayer.com>
2024-06-18 15:02:13 -0500

    initial commit

---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,19 @@
+const fizzbuzz = require('../lib/fizzbuzz');
+
+describe('fizzbuzz', () => {
+  it('should return "Fizz" for multiples of 3', () => {
+
+  });
+
+  it('should return "Buzz" for multiples of 5', () => {
+
+  });
+
+  it('should return "FizzBuzz" for multiples of both 3 and 5', () => {
+
+  });
+
+  it('should return the number itself for non-multiples of 3 or 5', () => {
+    
+  });
+});
