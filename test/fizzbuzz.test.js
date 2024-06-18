deff9922721b619544db527937ea399fbc1568e7
Samantha Belano <samantha.belano@bayer.com>
2024-06-18 16:42:46 -0400

    Changed test and js file

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -2,18 +2,22 @@ const fizzbuzz = require('../lib/fizzbuzz');
 
 describe('fizzbuzz', () => {
   it('should return "Fizz" for multiples of 3', () => {
-
+    let result = fizzbuzz(3)
+    expect(result).toEqual("fizz")
   });
 
   it('should return "Buzz" for multiples of 5', () => {
-
+    let result = fizzbuzz(5)
+    expect(result).toEqual("buzz")
   });
 
   it('should return "FizzBuzz" for multiples of both 3 and 5', () => {
-
+    let result = fizzbuzz(15)
+    expect(result).toEqual("fizzbuzz")
   });
 
   it('should return the number itself for non-multiples of 3 or 5', () => {
-    
+    let result = fizzbuzz(23)
+    expect(result).toEqual(23)
   });
 });
